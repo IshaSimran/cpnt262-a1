409da431a7a9c8cd69cba513e6904f2c2c96a604
Isha Sidhu <ishasimranxo@gmail.com>
2021-02-19 17:55:48 -0700

    typo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 `use strict`;
-type="text/javascript"
+type="text/javascript";
 document.querySelector('.checkbox').addEventListener('change',()=>{
   document.querySelectorAll('.night-mode-available').forEach(ele=>{
     ele.classList.toggle('night');
-  })
+  });
 });
